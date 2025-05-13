df14d20d528783085753046d916c39bdf0a4ec68
quack979 <ari3lmichael@gmail.com>
2025-05-13 09:31:57 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,2 +1,10 @@
 //part A
 Public static 
+
+  /**
+* Takes at least one dog for a walk during the time specified by
+* hour, as described in part (a)
+* Preconditions: 0 <= hour <= 23
+* maxDogs > 0
+*/
+public int walkDogs(int hour)
