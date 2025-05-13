2579581466e29b160a6435bd4b99ffdbdaa47a72
quack979 <ari3lmichael@gmail.com>
2025-05-13 09:29:20 -0400

    Create maincode.java

---
+++ Q1/maincode.java
@@ -0,0 +1,22 @@
+public class DogWalkCompany
+{
+/**
+* Returns the number of dogs, always greater than 0, that are available
+* for a walk during the time specified by hour
+* Precondition: 0 <= hour <= 23
+*/
+public int numAvailableDogs(int hour)
+{ /* implementation not shown */ }
+
+/**
+* Decreases, by numberDogsWalked, the number of dogs available for a walk
+* during the time specified by hour
+* Preconditions: 0 <= hour <= 23
+* numberDogsWalked > 0
+*/
+public void updateDogs(int hour, int numberDogsWalked)
+{ /* implementation not shown */ }
+
+/* There may be instance variables, constructors,
+and methods that are not shown. */
+}
