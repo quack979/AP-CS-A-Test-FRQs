00ce3c0447e9f81b7c66d14257e963b480c621fa
quack979 <ari3lmichael@gmail.com>
2025-05-13 09:31:40 -0400

    Update zzzmaincode.java

--- Q1/zzzmaincode.java
+++ Q1/zzzmaincode.java
@@ -20,3 +20,40 @@ public void updateDogs(int hour, int numberDogsWalked)
 /* There may be instance variables, constructors,
 and methods that are not shown. */
 }
+
+public class DogWalker
+{
+/** The maximum number of dogs this walker can walk simultaneously
+per hour */
+private int maxDogs;
+
+/** The dog-walking company this dog walker is associated with */
+private DogWalkCompany company;
+
+/**
+* Assigns max to maxDogs and comp to company
+* Precondition: max > 0
+*/
+public DogWalker(int max, DogWalkCompany comp)
+{ /* implementation not shown */ }
+
+/**
+* Takes at least one dog for a walk during the time specified by
+* hour, as described in part (a)
+* Preconditions: 0 <= hour <= 23
+* maxDogs > 0
+*/
+public int walkDogs(int hour)
+{ /* to be implemented in part (a) */ }
+/**
+* Performs an entire dog-walking shift and returns the amount
+* earned, in dollars, as described in part (b)
+* Preconditions: 0 <= startHour <= endHour <= 23
+* maxDogs > 0
+*/
+public int dogWalkShift(int startHour, int endHour)
+{ /* to be implemented in part (b) */ }
+
+/* There may be instance variables, constructors,
+and methods that are not shown. */
+}
