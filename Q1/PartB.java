e4ffbca1b5bbaddca2199dbdf5ffcd8068f18d83
quack979 <ari3lmichael@gmail.com>
2025-05-13 09:32:22 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,7 @@
-
+/**
+* Performs an entire dog-walking shift and returns the amount
+* earned, in dollars, as described in part (b)
+* Preconditions: 0 <= startHour <= endHour <= 23
+* maxDogs > 0
+*/
+public int dogWalkShift(int startHour, int endHour)
