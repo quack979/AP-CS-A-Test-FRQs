feebd4d7ac2c7f8bb1ed093662296cddb6211c95
quack979 <ari3lmichael@gmail.com>
2025-05-18 20:18:58 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,10 +1,18 @@
 //part A
 Public static 
-
   /**
 * Takes at least one dog for a walk during the time specified by
 * hour, as described in part (a)
 * Preconditions: 0 <= hour <= 23
 * maxDogs > 0
 */
-public int walkDogs(int hour)
+public int walkDogs(int hour){
+   if(numAvailableDogs(hour)>=maxDogs){
+     updateDogs(hour, maxDogs);
+     return (int)(maxDogs);
+   }
+   else{
+     updateDogs(hour,numAvailableDogs(hour));
+     return (int)(numAvailableDogs(hour));
+   }
+}
