836aadc1fdf192846b5c9ba361fb02bebf65b1cb
quack979 <ari3lmichael@gmail.com>
2025-05-19 10:13:30 -0400

    Update answer.java

--- Q2/answer.java
+++ Q2/answer.java
@@ -1,1 +1,27 @@
+public class SignedText{
+  private String first;
+  private String last;
+  public SignedText(String fir,String las){
+    first=fir;
+    last=las;
+  }
 
+  public String getSignature (){
+    if(first.length()>0){
+      return first.substring(0,1)+"-"+last;
+    }else{
+      return last;
+    }
+  }
+  public String addSignature (String str){
+    String sign = getSignature();
+    int index = str.indexOf(sign);
+    if(index==0){
+      return str.substring(sign.length())+sign;
+    }else if(){
+      
+    }else{
+      
+    }
+  }
+}
