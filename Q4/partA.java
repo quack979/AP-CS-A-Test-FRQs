f192673eb39142a114a24a30c8e1809e6b6a7b01
quack979 <ari3lmichael@gmail.com>
2025-05-20 10:07:04 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,13 @@
-
+/**
+* Creates a two-dimensional array and fills it with random integers,
+* as described in part (a)
+* Precondition: numRows > 0; numCols > 0
+*/
+public SumOrSameGame(int numRows, int numCols){
+  puzzle= new int[numRows][numCols];
+  for(int k = 0; k<numRows; k++){
+    for)(int h = 0; h < numCols; h++){
+      puzzle[k][h]=(int)(Math.random()*9)+1;
+    }
+  }
+}
