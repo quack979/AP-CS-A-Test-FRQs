5c7d9caed4845730d356d8c2b0fbaa4814ee8468
quack979 <ari3lmichael@gmail.com>
2025-05-20 09:52:13 -0400

    Update partA.java

--- Q3/partA.java
+++ Q3/partA.java
@@ -1,1 +1,7 @@
-
+/** Initializes competitorList, as described in part (a) */
+public Round(String[] names){
+  competitorList = new ArrayList<Competitor>();
+  for(int rank = 0; rank<names.length; rank++){
+    competitorList.add(new Competitor(names[rank],rank+1));
+  }
+}
