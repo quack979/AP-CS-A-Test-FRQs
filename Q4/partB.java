22cc1057f3bbea598477ac9f0bad7d07cbb712e1
quack979 <ari3lmichael@gmail.com>
2025-05-20 10:11:09 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,1 +1,20 @@
-
+/**
+* Identifies and clears an element of puzzle that can be paired with
+* the element at the given row and column, as described in part (b)
+* Preconditions: row and col are valid row and column indices in puzzle.
+* The element at the given row and column is between 1 and 9, inclusive.
+*/
+public boolean clearPair(int row, int col){
+  int key = puzzle[row][col];
+  for(int k = row;k<puzzle.length;k++){
+    for(int h = 0; h<puzzle[0].length;h++){
+      int check = puzzle[k][h];
+      if(check==key || check + key == 10){
+        puzzle[row][col] = 0;
+        puzzle[k][h] = 0;
+        return true;
+      }
+    }
+  }
+  return false;
+}
